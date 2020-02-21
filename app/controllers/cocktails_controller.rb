d487793aedfd136144667c77965453d6040208ca
Joaquin Panelati <jpanelati@itba.edu.ar>
2020-02-21 18:14:20 -0300

    -

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,15 +4,14 @@ class CocktailsController < ApplicationController
   end
 
   def show
-  	@cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
-  	@cocktail = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def create
-
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
       redirect_to root_path
@@ -23,7 +22,7 @@ class CocktailsController < ApplicationController
 
   private
 
- def cocktail_params
-   params.require(:cocktail).permit(:name, :photo)
-end
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
 end
